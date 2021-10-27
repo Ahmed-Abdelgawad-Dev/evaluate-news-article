d1475f53bf3c486582da606f0d3bb31c7cd33c38
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-10-27 22:20:53 +0200

    updates

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,8 +1,11 @@
+
 import validatingURL from "./validatingURL";
 // importing axios like this for more autocomplete support
 const axios = require("axios").default;
 
-const postingData = async (url = "", data = {}) => {
+// an async function uses axios to post.
+const postingData = async (url = "nothing", data = {}) => {
+    // Parameters to succeed in talking to HTTP protocol. 
     const params = {
         mod: "cors",
         credentials: "same-origin",
@@ -10,28 +13,39 @@ const postingData = async (url = "", data = {}) => {
         body: JSON.stringify(data),
     }
     try {
+        // Try to post the data
         const response = await axios.post(url, data, params)
+        // Testing data that came back by axios
         console.log(response.data)
         return response.data    
+        // If not show me the error in the console
     } catch (error) {
         console.log(err)
     }
 }
 
-
+// Handling submit function 
 export default async function handleSubmit(e) {
+    //Preventing default refreshing by browser's nature
     e.preventDefault();
+    // Get the value of the article to evaluate.
     const articleURL = document.getElementById("article-url").value;
+    // If the article is valid
     if (validatingURL(articleURL)) {
+        //Invoking & using postingData func in case of having a valid URL link. 
         const data = await postingData("http://localhost:8081/apiPost", {
             url: articleURL,
         });
+        // Injecting the data into the DOM to see the result in the starting page.
         usingData({ data });
     } else {
+        // Alerting the user in case of wrong URL.
         alert("The Url you entered is not valid. Please enter a valid one!");
     }
 }
 const usingData = ({ data }) => {
+    // Injecting the result one by one using innerHTML 
+    // After targeting every element by id
     document.getElementById(
         "agreement"
     ).innerHTML = `Agreement Type: ${data.agreement.toLowerCase()}`;
@@ -49,25 +63,12 @@ const usingData = ({ data }) => {
     document.getElementById(
         "score_tag"
     ).innerHTML = `Score Tag: ${data.score_tag.toLowerCase()}`;
+    // Adding the status as an extra option.
     document.getElementById(
         "status-msg"
     ).innerHTML = `Status: ${data.status.msg}`;
 };
 
+//Exporting both functions except handleSubmit
+// as it's already exported by default
 export { postingData, usingData };
-
-// const fetchingData = async (url='', data={}) => {
-//     const resp  = await fetch(url, {
-//         method: 'POST',
-//         mod: 'cors',
-//         credentials: 'same-origin',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify(data)
-//     })
-//     try {
-//         return await resp.json()
-//     }
-//     catch (e) {
-//         console.log(e)
-//     }
-// }
--- src/server/index.js
+++ src/server/index.js
@@ -1,53 +1,60 @@
-const path = require('path')
-require('dotenv').config() // for environment variables
-const cors = require('cors') //cors to avoid cors-origin issue
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const path = require("path");
+require("dotenv").config(); // for environment variables
+const cors = require("cors"); //cors to avoid cors-origin issue
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
 const bodyParser = require("body-parser"); //parser
-const axios = require('axios').default // axios to deal with node server
+const axios = require("axios").default; // axios to deal with node server
 
 //choosing whatever port we want.
-const PORT = 8081
+const PORT = 8081;
 // initializing app with express
-const app = express()
+const app = express();
 //Middleware
 app.use(cors());
-app.use(bodyParser.json())
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //choosing a static root
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    // choosing which file inside the distribution folder to target
-    res.sendFile(path.resolve('dist/index.html'))
-})
+app.get("/", function (req, res) {
+	// choosing which file inside the distribution folder to target
+	res.sendFile(path.resolve("dist/index.html"));
+});
 
 //Destructuring the environment variables
-const {MEANING_CLOUD_API_KEY, API_URL} = process.env
-console.log(' Use this key or your own =>   ',MEANING_CLOUD_API_KEY)
+const { MEANING_CLOUD_API_KEY, BASE_API_URL } = process.env;
+
+// These keys are being left in purpose for simplifying the running process
+console.log("Please use these key variables inside .env in your root dir if you don't have them.");
+console.log('MEANING_CLOUD_API_KEY=55bd733e899882529af33d85e89d1c1c')
+console.log('BASE_API_URL=https://api.meaningcloud.com/sentiment-2.1')
+
 // A function to use with posting.
 const apiPost = async (req, res) => {
-  const { body: { url } } = req;
-  const apiUrl = `${API_URL}?key=${MEANING_CLOUD_API_KEY}&url=${url}&lang=en`
-  const response = await axios.post(apiUrl);
-  const result = response.data;
-  res.send(result);
-}
+	const {
+		body: { url },
+	} = req;
+	const apiUrl = `${BASE_API_URL}?key=${MEANING_CLOUD_API_KEY}&url=${url}&lang=en`;
+	const response = await axios.post(apiUrl);
+	const result = response.data;
+	res.send(result);
+};
 // POST
-app.post('/apiPost', apiPost)
+app.post("/apiPost", apiPost);
 // GET
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+	res.send(mockAPIResponse);
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(PORT, (error) => {
-    if (error) throw new Error(error)
-    console.log(`Server listening on port ${PORT}!`)
-})
+	if (error) throw new Error(error);
+	console.log(`Server listening on port ${PORT}!`);
+});
 
 // export app to use it in the unit testing
-module.exports = {app}
+module.exports = { app };
