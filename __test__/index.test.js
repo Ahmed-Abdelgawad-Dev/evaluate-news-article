3c280f39cf065f53bf6c02098e56b5a250f4d028
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-10-27 22:35:55 +0200

    fix testing index bug

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,21 +2,6 @@ import { expect } from '@jest/globals';
 import {usingData,handleSubmit,postingData} from "../src/client/js/index";
 
 
-// test('usingData existence test', () => {
-//   expect(usingData).toBeDefined()
-// });
-// test('handleSubmit existence test',async () => {
-//   expect(handleSubmit).toBeDefined();
-// });
-test('postingData existence test', async () => {
-  expect.assertions(1);
-  const data = await response.data
-  expect(data.toBeDefined())
+test('usingData existence test', () => {
+  expect(usingData).toBeDefined()
 });
-
-// // async/await can be used.
-// it('works with async/await', async () => {
-//   expect.assertions(1);
-//   const data = await user.getUserName(4);
-//   expect(data).toEqual('Mark');
-// });
